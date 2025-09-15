f35b9c76247635a9900bbddc162ceb1223fcd1ec
Troy Temple <troy@tritiumillumination.com>
2025-09-14 21:51:35 -0500

    Removes extra import calls from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,7 @@ import Footer from "./components/Footer";
 import AccessGate from "./components/AccessGate";
 import PromptFrameworkBuilder from "./components/PromptFrameworkBuilder";
 import WorkshopBanner from "./components/WorkshopBanner";
-import VibePanel from "./components/VibePanel";
-import NaturalLanguageIntake, { IntakeResult } from "./NaturalLanguageIntake";
+
 
 export default function Home() {
   return (
